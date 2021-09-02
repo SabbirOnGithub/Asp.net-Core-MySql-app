c4cb9063b109d8a507b548f9774623cbcbde2f48
Sabbir <sabbir.ahmed.dev@gmail.com>
2021-09-02 13:01:50 +0600

    initial commit

---
+++ AccountOwnerServer/AccountOwnerServer/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace AccountOwnerServer
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Owner, OwnerDto>();
+            CreateMap<Account, AccountDto>();
+            CreateMap<OwnerForCreationDto, Owner>();
+        }
+    }
+}
---
+++ AccountOwnerServer/Contracts/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Models;
+
+namespace Contracts
+{
+    public interface IOwnerRepository: IRepositoryBase<Owner>
+    {
+        IEnumerable<Owner> GetAllOwners();
+        Owner GetOwnerById(Guid ownerId);
+        Owner GetOwnerWithDetails(Guid ownerId);
+        void CreateOwner(Owner owner);
+    }
+}
